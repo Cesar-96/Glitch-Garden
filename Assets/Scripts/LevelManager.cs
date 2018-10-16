71c8f463404c6a327ea0ece46574ec904509b1f2
Pablo Yucra <cesar.yucra12@gmail.com>
2018-10-15 19:27:00 -0500

    first commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+	public float autoLoadNextLevelAfter;
+
+	void Start()
+	{
+		if (autoLoadNextLevelAfter == 0)
+		{
+			Debug.Log("Level auto load disabled, use a positive number is seconds");
+		}
+		else
+		{
+			Invoke("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name)
+	{
+		Debug.Log("New Level load" + name);
+		Application.LoadLevel(name);
+	}
+
+	public void QuitRequest()
+	{
+		Debug.Log("Quit Requested");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel()
+	{
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
